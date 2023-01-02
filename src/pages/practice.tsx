55eb1bfbd6a90599ca1fb9eb3c0b2d3c4e97dfde
Rino Nakagawara <nakagawara@reclick.co.jp>
2023-01-02 17:45:36 +0900

    update: 過去制作の追加

--- src/pages/practice.tsx
+++ src/pages/practice.tsx
@@ -3,65 +3,7 @@ import { AppLayout } from "@/components/Layout";
 import { List } from "@/components/List";
 import { Detail } from "@/components/Detail";
 import { IItem } from "@/types/Item";
-
-const practiceData = [
-  {
-    name: "株式会社 FiNC Technologies 採用情報ページ",
-    portforioUrl: "https://nrwgkn.github.io/reproduction-site-finc_recruit/",
-    category: ["模写コーディング", "JavaScript", "jQuery", "Sass"],
-    url: "https://company.finc.com/recruit/",
-    thumbnail: "../img/practice/img_reproduction_finc.jpg",
-  },
-  {
-    name: "Nature Remo LP",
-    portforioUrl: "https://nrwgkn.github.io/reproduction-site-remo_lp/",
-    category: ["模写コーディング", "JavaScript", "Sass"],
-    url: "https://nature.global/jp/landing-page-dm-re/",
-    thumbnail: "../img/practice/img_reproduction_remo.jpg",
-  },
-  {
-    name: "東京上野ワンダラー（Tokyo Ueno Wonderer）",
-    portforioUrl: "https://nrwgkn.github.io/reproduction-site-ueno_wonderer/",
-    category: ["模写コーディング", "JavaScript", "jQuery", "Sass"],
-    url: "https://ueno-wonderer.jp/",
-    thumbnail: "../img/practice/img_reproduction_ueno.jpg",
-  },
-  {
-    name: "ベイク チーズタルト",
-    portforioUrl: "https://nrwgkn.github.io/reproduction-site-bake/",
-    category: ["模写コーディング", "JavaScript", "jQuery", "Sass"],
-    url: "https://cheesetart.com/",
-    thumbnail: "../img/practice/img_reproduction_bake.jpg",
-  },
-  {
-    name: "React ストップウォッチ",
-    portforioUrl: "https://nrwgkn.github.io/react-stopwatch/",
-    category: ["React.js"],
-    thumbnail: "../img/practice/img_react_stopwatch.png",
-    text: "",
-  },
-  {
-    name: "React 電卓",
-    portforioUrl: "https://nrwgkn.github.io/react-calculator/",
-    category: ["React.js"],
-    thumbnail: "../img/practice/img_react_calculator.png",
-    text: "",
-  },
-  {
-    name: "React メモ",
-    portforioUrl: "https://nrwgkn.github.io/react-memo/",
-    category: ["React.js"],
-    thumbnail: "../img/practice/img_react_memo.png",
-    text: "",
-  },
-  {
-    name: "Trending Posts",
-    portforioUrl: "https://trending-posts.netlify.app/",
-    category: ["React.js", "TypeScript", "Sass", "Netlify", "Twitter API"],
-    thumbnail: "../img/practice/img_react_twitterApi.png",
-    text: "",
-  },
-];
+import { practiceData } from "@/data";
 
 const Practice = () => {
   const [showData, setShowData] = useState<IItem | null>(null);
--- src/pages/works.tsx
+++ src/pages/works.tsx
@@ -3,59 +3,7 @@ import { AppLayout } from "@/components/Layout";
 import { List } from "@/components/List";
 import { Detail } from "@/components/Detail";
 import { IItem } from "@/types/Item";
-
-const worksData = [
-  {
-    name: "株式会社キャンディル",
-    category: [
-      "Responsive",
-      "JavaScript",
-      "jQuery",
-      "Sass",
-      "WordPress",
-      "Speed improvement",
-    ],
-    url: "https://www.candeal.co.jp/",
-    thumbnail: "../img/works/img_works_candeal.jpg",
-    text: "サイトの大幅改修に携わりました。既存ページの改修、新規で追加になったページの制作も対応しました。1ページあたりのメディア（主に画像）数が多いページが多く、読み込み速度が著しく下がることのないよう、PageSpeedInsight等活用し画像の最適化や遅延読み込みなど表示速度の部分も意識して制作を進めました。",
-  },
-  {
-    name: "ネットカフェダイス",
-    category: ["Responsive", "JavaScript", "Sass"],
-    url: "https://www.diskcity.co.jp/",
-    thumbnail: "../img/works/img_works_dice.jpg",
-  },
-  {
-    name: "【どらなび】バス運転手専門の就職・転職支援求人サイト",
-    category: ["Responsive", "JavaScript", "jQuery", "Photoshop"],
-    url: "https://www.bus-dnavi.com/",
-    thumbnail: "../img/works/img_works_drinavi.jpg",
-  },
-  {
-    name: "インプレックスアンドカンパニー株式会社　コーポレートサイト",
-    category: ["Responsive", "JavaScript", "Speed improvement"],
-    url: "https://www.imprexc.jp/",
-    thumbnail: "../img/works/img_works_imprexc.jpg",
-  },
-  {
-    name: "2020年秋プレミアムブリッジサロンLP",
-    category: ["Responsive", "JavaScript", "Photoshop"],
-    url: "https://www.bridge-salon.jp/premium_salon/2020autumn/",
-    thumbnail: "../img/works/img_works_bridgelp.jpg",
-  },
-  {
-    name: "リクリック コーポレートサイト",
-    category: ["Responsive", "JavaScript", "Sass", "Speed improvement"],
-    url: "https://www.reclick.co.jp/",
-    thumbnail: "../img/works/img_works_reclick.png",
-  },
-  {
-    name: "東京・錦糸町のメンバーズバー　CAMPO BAR",
-    category: ["JavaScript", "Sass", "Speed improvement"],
-    url: "http://campo-bar.com/",
-    thumbnail: "../img/works/img_works_bar.jpg",
-  },
-];
+import { worksData } from "@/data";
 
 const Works = () => {
   const [showData, setShowData] = useState<IItem | null>(null);
