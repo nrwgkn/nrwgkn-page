84111444782b62ab957617e5ab259595355a1852
Rino Nakagawara <nakagawara@reclick.co.jp>
2025-04-10 16:01:26 +0900

    fix: revert

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: "/works", // パス
-        headers: [
-          {
-            key: "Basic-Auth",
-            value: "13ba288l:a5RMseyk",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
